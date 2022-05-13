bb04dee98c1cec66c4c351023b76faad83a54c23
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2022-05-13 01:27:20 -0500

    Initial commit

---
+++ src/main/java/tv/galaxe/galaxesmp/GalaxeSMP.java
@@ -0,0 +1,17 @@
+package tv.galaxe.galaxesmp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GalaxeSMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
